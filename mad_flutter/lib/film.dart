92271a03c192481b565e0d61e02783b968ce64b3
AbakumovaOlga <31732995+AbakumovaOlga@users.noreply.github.com>
2022-04-18 23:03:24 +0400

    HW_2_noPro

--- mad_flutter/lib/film.dart
+++ mad_flutter/lib/film.dart
@@ -1,20 +1,32 @@
 import 'package:enum_to_string/enum_to_string.dart';
 
+abstract class FilmAbstract {
+  final String id;
+  final String title;
+  final String picture;
+  final double voteVerage;
+  final String releaseDate;
+  final String description;
+  final String language;
 
-abstract class FilmAbstract
-{
-  late final String id;
-  late final String title;
-  late final String picture;
-  late final double voteVerage;
-  late final String releaseDate;
-  late final String description;
-  late final String language;
-
+  const FilmAbstract(this.id, this.title, this.picture, this.voteVerage,
+      this.releaseDate, this.description, this.language);
 }
 
-class Film extends FilmAbstract with ConvertLanguage{
-  Film(String id, String title, String picture, double voteVerage, String releaseDate, String description, String language){
+class Film extends FilmAbstract with ConvertLanguage {
+  const Film(
+      {required String id,
+      required String title,
+      required String picture,
+      required double voteVerage,
+      required String releaseDate,
+      required String description,
+      required String language})
+      : super(
+            id, title, picture, voteVerage, releaseDate, description, language);
+
+  /*Film(String id, String title, String picture, double voteVerage, String releaseDate, String description, String language)
+      :super (id, title,picture,voteVerage,releaseDate,description,language){
     this.id=id;
     this.title=title;
     this.picture=picture;
@@ -22,35 +34,31 @@ class Film extends FilmAbstract with ConvertLanguage{
     this.releaseDate=releaseDate;
     this.description=description;
     this.language=language;
-  }
+  }*/
 
   @override
   String toString() {
-    // TODO: implement toString
-    return this.id+" "+this.title +" " +toPrettyString(convert(this.language))+" ";
+    return id + " " + title + " " + convert(language)!.toPrettyString(convert(language)) + " ";
   }
-
-
 }
 
-enum Language {
-  russian,
-  english
-}
+enum Language { russian, english }
 
-mixin ConvertLanguage{
-  Language? convert(String language){
+mixin ConvertLanguage {
+  Language? convert(String language) {
     return EnumToString.fromString(Language.values, language);
   }
 }
 
-extension PrettyLanguage on Film {
-  String toPrettyString(Language? language){
+extension PrettyLanguage on Language {
+  String toPrettyString(Language? language) {
     switch (language) {
-      case Language.russian : return "Русский";
-      case Language.english : return "Английский";
-      default: return "Null";
-
+      case Language.russian:
+        return "Русский";
+      case Language.english:
+        return "Английский";
+      default:
+        return "Null";
     }
   }
 }
--- mad_flutter/lib/main.dart
+++ mad_flutter/lib/main.dart
@@ -1,42 +1,128 @@
-
-import 'dart:ffi';
+import 'package:flutter/material.dart';
 
 import 'film.dart';
-
-Future<void> main() async {
-  // runApp(MyApp());
-
+/*void main() {
   List<Film> originalFilms = [
-    new Film("id1", "title1", "picture1", 1.1, "releaseDate1", "description1",
-        "russian"),
-    new Film("id2", "title2", "picture2", 2.2, "releaseDate2", "description2",
-        "russian"),
-    new Film("id3", "title3", "picture3", 3.3, "releaseDate3", "description3",
-        "english")
+    new Film(
+        id: "id1",
+        title: "title1",
+        picture: "picture1",
+        voteVerage: 1.1,
+        releaseDate: "releaseDate1",
+        description: "description1",
+        language: "russian"),
+    new Film(
+        id: "id2",
+        title: "title2",
+        picture: "picture2",
+        voteVerage: 2.2,
+        releaseDate: "releaseDate2",
+        description: "description2",
+        language: "russian"),
+    new Film(
+        id: "id3",
+        title: "title3",
+        picture: "picture3",
+        voteVerage: 3.3,
+        releaseDate: "releaseDate3",
+        description: "description3",
+        language: "english")
   ];
 
-  Future<List<Film>> listFilms() async{
+
+  Future<List<Film>> listFilms() async {
     return originalFilms;
   }
 
   Future<String> stringFilms(List<Film> films) async {
     String res = "";
     films.forEach((element) {
-      res += (element.toString())+"\n";
+      res += (element.toString()) + "\n";
     });
     return res;
   }
 
-  List<Film> filterFilmsVoteVerage(List<Film> films, Double param) {
-    return films.where((element) => element.voteVerage>param).toList();
+  List<Film> filterFilmsVoteVerage(List<Film> films, double param) {
+    return films.where((element) => element.voteVerage > param).toList();
   }
 
-  /* List<Film> filterFilmsCustom (List<Film> films, String param){
 
-  }*/
-  print(await listFilms());
-  print('\n');
-  print(await stringFilms(originalFilms));
-  print('\n');
-  print(filterFilmsVoteVerage(originalFilms,2));
+  runApp(MyApp());
+}*/
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "_key",
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('To do list'),
+          centerTitle: true,
+        ),
+        body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Flexible(
+                  child: FilmWidget("title1", "picture1", Language.english)),
+              Flexible(
+                  child: FilmWidget("title2", "picture2", Language.russian))
+            ]),
+      ),
+    );
+  }
+}
+
+class FilmWidget extends StatefulWidget {
+  String _title;
+  String _picture;
+  Language _language;
+
+  @override
+  _FilmWidget createState() => _FilmWidget(_title, _picture, _language);
+
+  FilmWidget(this._title, this._picture, this._language);
+}
+
+class _FilmWidget extends State<FilmWidget> {
+  String _title;
+  String _picture;
+  Language _language;
+
+  _FilmWidget(this._title, this._picture, this._language);
+
+  refresh() {
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Row(children: [
+        Text(
+          _picture,
+          textAlign: TextAlign.center,
+          overflow: TextOverflow.ellipsis,
+          style: const TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+        ),
+        Image.asset(
+          "assets/images/$_picture.png",
+          height: 100,
+          width: 100,
+        ),
+        Text(
+          _language.toPrettyString(_language),
+          textAlign: TextAlign.center,
+          overflow: TextOverflow.ellipsis,
+          style: const TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+        ),
+      ]),
+    );
+  }
 }
